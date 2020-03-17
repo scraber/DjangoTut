67b2ac4dedf25d8a46e242a7a8157d0892399af2
Mateusz Kacprzak <mrscraber@gmail.com>
2020-03-17 10:27:54 +0100

    D5: Add login functionality

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -8,9 +9,15 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('boardgames-home')
+            # username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Account has been created! You are able to log in now')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
