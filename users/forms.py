217212f7d3fb8be08bb64639138ee53de8cfc836
Mateusz Kacprzak <mrscraber@gmail.com>
2020-03-17 12:49:55 +0100

    D6: Add user profile and profile picture on create

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    display_name = forms.CharField(label='Displayed name on webpage')
+    display_name = forms.CharField(label='Display name')
+    image = forms.ImageField(label='Profile picture', required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'display_name', 'email', 'password1', 'password2']
+        fields = ['username', 'display_name', 'image',
+                  'email', 'password1', 'password2']
--- users/models.py
+++ users/models.py
@@ -4,4 +4,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     display_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
