658a6009e8278a692c8601bfcbf96309ae841f6a
Mateusz Kacprzak <mrscraber@gmail.com>
2020-03-23 17:58:10 +0100

    D10: Add reset password functionality

--- django_boardgames/settings.py
+++ django_boardgames/settings.py
@@ -128,3 +128,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "boardgames-home"
 LOGIN_URL = "login"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Remember to allow lesssecure apps on google account settings
+# https://myaccount.google.com/lesssecureapps
+EMAIL_HOST_USER = "xxx@gmail.com"
+EMAIL_HOST_PASSWORD = "xxx"
--- django_boardgames/urls.py
+++ django_boardgames/urls.py
@@ -20,6 +20,7 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", user_views.register, name="register"),
@@ -33,6 +34,32 @@ urlpatterns = [
         "logout/",
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout",
+    ),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="users/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
     path("", include("boardgames.urls")),
 ]
